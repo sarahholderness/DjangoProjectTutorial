4b484042651655170820025c1203da6ff1442eec
Sarah Buchanan <sbuchanan@codeschool.com>
2016-02-05 16:37:24 -0500

    First commit

---
+++ polls/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Question, Choice
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+admin.site.register(Choice)
